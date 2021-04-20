9d4b3d9cb6b133b65332d1b9814fe5e732c264a5
Jeremy Germenis <jgermeni@ucsc.edu>
2021-04-20 14:57:34 -0700

    Final Height Push

--- Software/Barone2Jeremy.X/Height.c
+++ Software/Barone2Jeremy.X/Height.c
@@ -13,64 +13,50 @@
 
 #define MAX_MEASUREMENTS 10 //changes size of array
 
-static double height[3][MAX_MEASUREMENTS]; //array and indedx to store measurement values
+static double height[4][MAX_MEASUREMENTS]; //array and indedx to store measurement values
 //arrays 1 and 2 are for front sensors, row 3 is for bottom sensor
-static int index_bottom;
-static int index_front_left;
-static int index_front_right;
+static int index1;
 
-//inputs: Sensor reading from bottom ultrasonic
-//returns: Average of last 10 readings
+//inputs: Sensor readings from ultrasonics(m1 = bottom, m2 = front left, m3 = front right, m4 = front)
+//returns: Actual Reading
 
-double Track_Height_Bottom(double measurement)
+double Track_Height(double m1, double m2, double m3, double m4)
 {
-    height[2][index_bottom] = measurement; //puts new measurement into array
-    index_bottom = (index_bottom + 1) % MAX_MEASUREMENTS; //increments index
+    height[0][index1] = m1; //puts new measurements from each sensor into array
+    height[1][index1] = m2;
+    height[2][index1] = m3;
+    height[3][index1] = m4;
+    index1 = (index1 + 1) % MAX_MEASUREMENTS; //increments index
     int i;
-    double avg = 0;
-    for (i = 0; i < MAX_MEASUREMENTS; i++) { //returns the average height measured recently
-        avg += height[2][i];
+    double avg1, avg2, avg3, avg4;
+    for (i = 0; i < MAX_MEASUREMENTS; i++) { //avg height of every row is calcualted
+        avg1 += height[0][i];
+        avg2 += height[1][i];
+        avg3 += height[2][i];
+        avg4 += height[3][i];
     }
-    return (avg / MAX_MEASUREMENTS);
+    avg1 = avg1 / MAX_MEASUREMENTS;
+    avg2 = avg2 / MAX_MEASUREMENTS;
+    avg3 = avg3 / MAX_MEASUREMENTS;
+    avg4 = avg4 / MAX_MEASUREMENTS;
+    double h = Actual_Height(avg1, avg2, avg3, avg4);
+    return (h);
 }
-//inputs: Sensor reading from front left facing ultrasonic
-//returns: Average of last 10 readings from sensor
+//inputs: Avg reading from each ultrasonic(m1 = bottom, m2 = front left, m3 = front right, m4 = front)
+//returns: Actual Height based on trigonometry
 
-double Track_Height_Front_Left(double measurement)
+double Actual_Height(double m1, double m2, double m3, double m4)
 {
-    height[0][index_front_left] = measurement; //puts new measurement into array
-    index_front_left = (index_front_left + 1) % MAX_MEASUREMENTS; //increments index
-    int i;
-    double avg = 0;
-    for (i = 0; i < MAX_MEASUREMENTS; i++) { //returns the average height measured recently
-        avg += height[0][i] * 0.707; //divides by sqrt(2) for height since 45 degree angle)
+    if ((m1 < (m2 * .707)) && (m1 < (m3 * .707))) {
+        return m1; //if bottom sensor height is lower than front, going at a decline
+    } 
+    if (m4 < m2 || m4 < m3){
+        return m1; //if front sensor is lower than the front angled sensors, obstacle in the way
     }
-    return (avg / MAX_MEASUREMENTS);
-}
-
-//inputs: Sensor reading from front right facing ultrasonic sensor
-//returns: Average of last 10 readings from sensor
-
-double Track_Height_Front_Right(double measurement)
-{
-    height[1][index_front_right] = measurement; //puts new measurement into array
-    index_front_right = (index_front_right + 1) % MAX_MEASUREMENTS; //increments index
-    int i;
-    double avg = 0;
-    for (i = 0; i < MAX_MEASUREMENTS; i++) { //returns the average height measured recently
-        avg += height[1][i] * 0.707; //divides by sqrt(2) for height since 45 degree angle)
-    }
-    return (avg / MAX_MEASUREMENTS);
-}
-
-double Actual_Height(double bottom, double right, double left)
-{
-    if ((bottom < left) && (bottom < right)) {
-        return bottom;
-    } else if (left < right) {
-        return left;
+    if (m2 < m3) { //lastly, if going at an incline, front angled sensors should be chosen based on least height
+        return (m2 * .707);
     } else {
-        return right;
+        return (m3 * .707);
     }
 }
 //inputs: scalar double value to change entire array by
@@ -85,7 +71,7 @@ int Update_Values(double scalar, int row)
     return (1);
 }
 
-#define TRACK_HEIGHT_TEST
+//#define TRACK_HEIGHT_TEST
 #ifdef TRACK_HEIGHT_TEST
 #include "BOARD.h"
 
@@ -94,22 +80,21 @@ int main(void)
     BOARD_Init();
     int i;
     srand(time(NULL));
-    double rand1, rand2, rand3;
-    double height_bottom, height_front_right, height_front_left, actual_height;
+    double rand1, rand2, rand3, rand4;
+    double actual_height;
     while (1) {
         for (i = 0; i < 200000; i++) {
         }
         rand1 = rand() % 400; //puts random measurement into array
         rand2 = rand() % 400;
         rand3 = rand() % 400;
-        height_bottom = Track_Height_Bottom(rand1);
-        height_front_right = Track_Height_Front_Right(rand2);
-        height_front_left = Track_Height_Front_Left(rand3);
-        actual_height = Actual_Height(height_bottom, height_front_right, height_front_left);
+        rand4 = rand() % 400;
+        actual_height = Actual_Height(rand1, rand2, rand3, rand4);
 
-        printf("Bottom Avg: %f   Cur: %f\n", height_bottom, rand1); //prints avg measurement
-        printf("FrontR Avg: %f   Cur: %f\n", height_front_right, rand2 / .707);
-        printf("FrontL Avg: %f   Cur: %f\n", height_front_left, rand3 / .707);
+        printf("Bottom Cur: %f\n", rand1); //prints avg measurement
+        printf("FrontL Cur: %f\n", rand2 / .707);
+        printf("FrontR Cur: %f\n", rand3 / .707);
+        printf("Front  Cur: %f\n", rand4);
         printf("Actual: %f\n", actual_height);
 
     }
--- Software/Barone2Jeremy.X/Height.h
+++ Software/Barone2Jeremy.X/Height.h
@@ -1,13 +1,9 @@
 #ifndef HEIGHT_H
 #define HEIGHT_H
 
-double Track_Height_Bottom(double measurement);
+double Track_Height(double m1, double m2, double m3, double m4);
 
-double Track_Height_Front_Left(double measurement);
-
-double Track_Height_Front_Right(double measurement);
-
-double Actual_Height(double bottom, double right, double left);
+double Actual_Height(double m1, double m2, double m3, double m4);
 
 int Update_Values(double scalar, int row);
 
